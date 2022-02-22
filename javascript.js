3aabe2fdd211cfb8510f90b1f4782563c87e03a1
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-02-21 21:59:19 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-onbeforeunload = function(){localStorage.x=1}
-setTimeout(function(){
-    while(1)location.reload(1)
-  }, 1000)
+//onbeforeunload = function(){localStorage.x=1}
+//setTimeout(function(){
+//    while(1)location.reload(1)
+//  }, 1000)
+window.open("https://triton-z.github.io/IQ-Test/");
